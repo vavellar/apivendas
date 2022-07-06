7323790d50da1fb6b604d30632ed7ffb22077e9b
victorcandeias <victorcandeias@wine.com.br>
2022-07-06 14:04:58 -0300

    feat: creating Product entity

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
+
+export default Product;
