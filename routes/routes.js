22fc5872dcb1844324a8d543461f155476e9d061
victorcandeias <victorcandeias@wine.com.br>
2022-07-05 22:04:10 -0300

    feat: remake folder structure

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-var express = require("express")
-var app = express();
-var router = express.Router();
-var HomeController = require("../controllers/HomeController");
-
-router.get('/', HomeController.index);
-
-module.exports = router;
