20f870dcfbb0113ee150185427cfbaef037a3d0b
victorcandeias <victorcandeias@wine.com.br>
2022-07-06 15:29:10 -0300

    feat: creating UpdateProductService

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,40 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+
+    const product = await productRepository.findOne(id);
+    const productExists = await productRepository.findByName(name);
+
+    if (!product) throw new AppError('Product not found');
+
+    if (productExists && name !== product.name)
+      throw new AppError('There is already one product with this name');
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productRepository.save(product);
+
+    return product;
+  }
+}
+
+export default UpdateProductService;
