c6b7088a305a2988d6be9910559c217056375329
victorcandeias <victorcandeias@wine.com.br>
2022-07-07 15:22:04 -0300

    refactor: moving isAuthenticated middlware to shared folder

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
-import isAuthenticated from '../middlewares/isAuthenticated';
+import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
 import UserValidator from '../validators/UserValidator';
 
 const usersRouter = Router();
