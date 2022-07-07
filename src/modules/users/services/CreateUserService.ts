9440fd6dcc7453dbfc92023daa271e9f687010be
victorcandeias <victorcandeias@wine.com.br>
2022-07-07 09:22:08 -0300

    feat: using bcrypt to encrypt password

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -17,10 +18,12 @@ export class CreateUserService {
 
     if (emailExists) throw new AppError('Email already in use');
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
