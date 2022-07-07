485d3f5c471307926c1c077c44680865da5df3e2
victorcandeias <victorcandeias@wine.com.br>
2022-07-07 17:20:45 -0300

    feat: creating UserAvatarController

---
+++ src/modules/users/controllers/UserAvatarController.ts
@@ -0,0 +1,18 @@
+import AppError from '@shared/errors/AppError';
+import { Request, Response } from 'express';
+import { UpdateUserAvatarService } from '../services/UpdateUserAvatarService';
+
+export default class UserAvatarController {
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateAvatar = new UpdateUserAvatarService();
+
+    if (!request.file) throw new AppError('Please, provide a image');
+
+    const user = await updateAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    return response.json(user);
+  }
+}
