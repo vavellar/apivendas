79e7cd5af33ab7ec577da54eb9cb452ad1e29948
victorcandeias <victorcandeias@wine.com.br>
2022-07-06 22:18:00 -0300

    feat: creating UsersController

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { CreateUserService } from '../services/CreateUserService';
+import { ListAllUsersService } from '../services/ListAllUsersService';
+
+export default class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listAllUsers = new ListAllUsersService();
+
+    const users = listAllUsers.execute();
+
+    return response.json(users);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+}
