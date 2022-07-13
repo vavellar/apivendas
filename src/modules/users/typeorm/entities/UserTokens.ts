48ffc360f097488bb12cc32f9ac4566a1d26b335
victorcandeias <victorcandeias@wine.com.br>
2022-07-13 09:47:38 -0300

    feat: creating UserTokens entity

---
+++ src/modules/users/typeorm/entities/UserTokens.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
+
+export default UserToken;
