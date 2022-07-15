5c1973dcbed804e5ab2af46c330e99da60755012
victorcandeias <victorcandeias@wine.com.br>
2022-07-15 07:34:22 -0300

    feat: creating SendForgotPasswordEmailService

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+export class SendForgetPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) throw new AppError('User does not exists.');
+
+    const token = await userTokensRepository.generate(user.id);
+
+    console.log(token);
+  }
+}
