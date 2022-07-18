3bb2dce9bcde12382fe2b7b5c2b1238a6ca45367
victorcandeias <victorcandeias@wine.com.br>
2022-07-18 18:34:28 -0300

    feat: creating ShowProfileService

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,20 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+export class ShowProfileService {
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UsersRepository);
+
+    const user = await userRepository.findById(user_id);
+
+    if (!user) throw new AppError('User not found.');
+
+    return user;
+  }
+}
