d2901f70db86fc17d60e2a0c8b34314b4ba47592
victorcandeias <victorcandeias@wine.com.br>
2022-07-19 09:50:43 -0300

    feat: creating OrdersProducts entity

---
+++ src/modules/orders/typeorm/entities/OrdersProducts.ts
@@ -0,0 +1,38 @@
+import Product from '@modules/products/typeorm/entities/Product';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import Order from './Order';
+
+@Entity('order_products')
+class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Order, order => order.order_products)
+  @JoinColumn({ name: 'order_id' })
+  order: Order;
+
+  @ManyToOne(() => Product, product => product.order_products)
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
+
+export default OrdersProducts;
