69c35978ef18d0550fba9a4675769d4dcaacfc9a
victorcandeias <victorcandeias@wine.com.br>
2022-07-19 09:30:07 -0300

    feat: creating migrations

---
+++ src/shared/typeorm/migrations/1658230284836-AddOrderIdToOrdersProducts.ts
@@ -0,0 +1,39 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class AddOrderIdToOrdersProducts1658230284836
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'order_id',
+        type: 'uuid',
+        isNullable: true,
+        generationStrategy: 'uuid',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrdersProductsOrder',
+        columnNames: ['order_id'],
+        referencedTableName: 'orders',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders_products', 'OrdersProductsOrder');
+
+    await queryRunner.dropColumn('orders_products', 'order_id');
+  }
+}
