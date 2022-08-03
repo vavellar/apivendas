d10e7fb750313dc2438fe39479f63d2f30c580ce
victorcandeias <victorcandeias@wine.com.br>
2022-08-03 14:47:57 -0300

    fix: column name on customers table

--- src/modules/customers/typeorm/entities/Customer.ts
+++ src/modules/customers/typeorm/entities/Customer.ts
@@ -3,6 +3,7 @@ import {
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('customers')
@@ -19,7 +20,7 @@ class Customer {
   @CreateDateColumn()
   created_at: Date;
 
-  @CreateDateColumn()
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
